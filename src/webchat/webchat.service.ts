df36360cba0328c3d0f133d79b6611e9614d1f9c
haruna2004 <harunafaruk2004@gmail.com>
2025-03-28 09:53:49 +0100

    update

--- src/webchat/webchat.service.ts
+++ src/webchat/webchat.service.ts
@@ -1,28 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { streamText } from 'ai';
+// import { streamText } from 'ai';
 import { ClientMessage } from './webchat.types';
 import { Response } from 'express';
 import { PAY_SYSTEM_PROMPT } from './ai/prompt';
-import { azure } from '@ai-sdk/azure';
-// import { createOpenAI } from '@ai-sdk/openai';
+// import { azure } from '@ai-sdk/azure';
 import { createTools } from './ai/aitools';
+import { AiService } from './ai/ai.service';
 
-// const groq = createOpenAI({
-//   baseURL: 'https://api.groq.com/openai/v1',
-//   apiKey: process.env.GROQ_API_KEY,
-//   compatibility: 'strict',
-// });
 
 @Injectable()
 export class WebchatService {
+  constructor(private readonly aiService: AiService) {}
+
   process(clientMessage: ClientMessage, res: Response) {
     const lastMessage =
       clientMessage.messages[clientMessage.messages.length - 1];
-    console.log('User: ', lastMessage.content);
+    console.log('User: ', lastMessage.content); 
 
-    const result = streamText({
-      // model: groq.chat('llama-3.3-70b-versatile'),
-      model: azure.chat('gpt-4o'),
+    return this.aiService.streamResponse({
       messages: clientMessage.messages,
       system: PAY_SYSTEM_PROMPT,
       tools: createTools(
@@ -30,11 +25,12 @@ export class WebchatService {
         clientMessage.brassAccountId,
       ),
       maxSteps: 10,
-      onFinish(event) {
-        console.log('Assistant: ', event.text);
-      },
-    });
-
-    return result.pipeDataStreamToResponse(res);
+    }, res)
   }
+
+  processBulk(message: string) {
+    console.log(message)
+    return Response.json({ message: 'Handle Bulk Logic' })
+  }
+
 }
